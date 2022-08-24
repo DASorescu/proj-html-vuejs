7607028199d7a2a190a36729e4cc2da61dc7b776
github_DASorescu <srsdln97@gmail.com>
2022-08-24 12:26:29 +0200

    assets-maincomponents-mainscss

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div >
-
+    <MainHeader/>
+    <TheMain/>
+    <MainFooter/>
   </div>
 </template>
 
 <script>
 
-
+import MainHeader from './components/MainHeader.vue';
+import TheMain from './components/TheMain.vue';
+import MainFooter from './components/MainFooter.vue';
 export default {
   name: 'App',
   components: {
-
+    MainHeader,
+    TheMain,
+    MainFooter,
   }
 }
 </script>
